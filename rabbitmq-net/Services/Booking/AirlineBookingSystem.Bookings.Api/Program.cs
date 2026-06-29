c4965afcb36bc0c5077d693dd1e9f0a27cdc326d
Carlos Martins <lemos_oliveiraa@hotmail.com>
2026-06-29 18:03:24 -0300

    feat: setup booking creation event

--- rabbitmq-net/Services/Booking/AirlineBookingSystem.Bookings.Api/Program.cs
+++ rabbitmq-net/Services/Booking/AirlineBookingSystem.Bookings.Api/Program.cs
@@ -1,8 +1,11 @@
 using System.Data;
 using System.Reflection;
+using AirlineBookingSystem.Bookings.Application.Consumers;
 using AirlineBookingSystem.Bookings.Application.Handlers;
 using AirlineBookingSystem.Bookings.Core.Repositories;
 using AirlineBookingSystem.Bookings.Infrastructure.Repositories;
+using AirlineBookingSystem.BuildingBlocks.Common;
+using MassTransit;
 using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,6 +26,21 @@ var assemblies = new[]
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(assemblies));
 
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<NotificationEventConsumer>();
+
+    config.UsingRabbitMq((context, cfg) =>
+        {
+            cfg.Host(builder.Configuration["EventBusSettings:Host"]);
+            cfg.ReceiveEndpoint(EventBusConstant.NotificationSentQueue, c =>
+            {
+                c.ConfigureConsumer<NotificationEventConsumer>(context);
+            });
+        }
+    );
+});
 
 builder.Services.AddScoped<IDbConnection>(_ =>
     new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
