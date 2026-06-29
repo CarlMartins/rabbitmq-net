2136cf93aa7fb5c4f54359fdf87a04584aefc41a
Carlos Martins <lemos_oliveiraa@hotmail.com>
2026-06-29 17:39:38 -0300

    feat: add and setup Swagger

--- rabbitmq-net/Services/Booking/AirlineBookingSystem.Bookings.Api/Program.cs
+++ rabbitmq-net/Services/Booking/AirlineBookingSystem.Bookings.Api/Program.cs
@@ -10,8 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var assemblies = new[]
 {
@@ -32,7 +32,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Airline Booking System - Bookings API v1");
+    });
 }
 
 app.UseHttpsRedirection();
