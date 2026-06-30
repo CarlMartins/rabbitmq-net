b1ca57d2438526b8d1dcc1fa5a19ac8788184c80
Carlos Martins <lemos_oliveiraa@hotmail.com>
2026-06-29 21:39:02 -0300

    feat: setup payment processed event

--- rabbitmq-net/Services/Payment/AirlineBookingSystem.Payments.Api/Program.cs
+++ rabbitmq-net/Services/Payment/AirlineBookingSystem.Payments.Api/Program.cs
@@ -1,8 +1,11 @@
 using System.Data;
 using System.Reflection;
+using AirlineBookingSystem.BuildingBlocks.Common;
+using AirlineBookingSystem.Payments.Application.Consumers;
 using AirlineBookingSystem.Payments.Application.Handlers;
 using AirlineBookingSystem.Payments.Core.Repositories;
 using AirlineBookingSystem.Payments.Infrastructure.Repositories;
+using MassTransit;
 using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,6 +26,19 @@ var assemblies = new[]
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(assemblies));
 
 builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<FlightBookedConsumer>();
+
+    config.UsingRabbitMq((context, cfg) =>
+        {
+            cfg.Host(builder.Configuration["EventBusSettings:Host"]);
+            cfg.ReceiveEndpoint(EventBusConstant.FlightBookedQueue,
+                c => { c.ConfigureConsumer<FlightBookedConsumer>(context); });
+        }
+    );
+});
 
 builder.Services.AddScoped<IDbConnection>(_ =>
     new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
